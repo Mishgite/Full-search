d3be222d7ab3bd51bcaf06ad07622ee875b4b795
Mishgite <7594201@gmail.com>
2024-02-01 08:14:55 +0300

    вывод изображения через pygame

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-from io import BytesIO
 import requests
-from PIL import Image
+import os
+import pygame
 from map_scale_utils import get_scale_params
 
 
@@ -11,9 +11,17 @@ def show_map(object_longitude, object_latitude):
 
     map_api_server = "http://static-maps.yandex.ru/1.x/"
     response = requests.get(map_api_server, params=map_params)
-
-    img = Image.open(BytesIO(response.content))
-    img.show()
+    map_file = "map.png"
+    with open(map_file, "wb") as file:
+        file.write(response.content)
+    pygame.init()
+    screen = pygame.display.set_mode((600, 450))
+    screen.blit(pygame.image.load(map_file), (0, 0))
+    pygame.display.flip()
+    while pygame.event.wait().type != pygame.QUIT:
+        pass
+    pygame.quit()
+    os.remove(map_file)
 
 
 def get_object_coordinates(toponym):
